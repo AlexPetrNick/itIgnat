ac320da792f32c7fa43833e4aa664ce700936b92
Alex2 <you@example.com>
2021-12-22 19:11:58 +0300

    fix bugs from hw5

--- src/p2-homeworks/h5/pages/JuniorPlus.tsx
+++ src/p2-homeworks/h5/pages/JuniorPlus.tsx
@@ -3,11 +3,12 @@ import {HW1} from '../../h1/HW1'
 import HW2 from '../../h2/HW2'
 import HW3 from '../../h3/HW3'
 import HW4 from '../../h4/HW4'
+import HW11 from "../../h11/HW11";
 
 export const JuniorPlus = () => {
     return (
         <div>
-            <HW3/>
+            <HW11/>
         </div>
     )
 }
