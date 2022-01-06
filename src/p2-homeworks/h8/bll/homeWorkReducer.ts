24d19059aa1a0c8d070d564a368c56dc4089b775
Alex2 <you@example.com>
2022-01-06 15:51:41 +0300

    add test hw8

--- src/p2-homeworks/h8/bll/homeWorkReducer.ts
+++ src/p2-homeworks/h8/bll/homeWorkReducer.ts
@@ -1,13 +1,37 @@
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export type stateType = {
+    _id: number
+    name: number
+    age: number
+}
+export type ActionType = {
+    type: 'sort' | 'check'
+    payload: string | number
+}
+
+export type homeWorkReducerType = (state: stateType[], action: ActionType) => stateType[]
+
+
+export const homeWorkReducer:homeWorkReducerType = (state, action) => {
+    let copyState = [...state]
     switch (action.type) {
         case 'sort': {
-            // need to fix
-            return state
+            switch (action.payload) {
+                case 'up': {
+                    copyState.sort((a, b) => b.age - a.age)
+                    return  copyState
+                }
+                case 'down': {
+                    copyState.sort((a, b) => a.age - b.age)
+                    return  copyState
+                }
+                default:
+                    return copyState
+            }
         }
         case 'check': {
-            // need to fix
-            return state
+            return copyState.filter(u => u.age > action.payload)
         }
-        default: return state
+        default:
+            return copyState
     }
 }
