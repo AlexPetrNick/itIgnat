67b3df20719e4e97eabd42f06927daa38a8950d4
Alex2 <you@example.com>
2022-01-06 18:25:12 +0300

    completed hw9 main, work alt

--- src/p2-homeworks/h9/Clock.tsx
+++ src/p2-homeworks/h9/Clock.tsx
@@ -1,49 +1,66 @@
 import React, {useState} from 'react'
 import SuperButton from '../h4/common/c2-SuperButton/SuperButton'
+import s from './Clock.module.css'
+
+
+
 
 function Clock() {
     const [timerId, setTimerId] = useState<number>(0)
-    const [date, setDate] = useState<Date>()
+    const [date, setDate] = useState<string>('')
     const [show, setShow] = useState<boolean>(false)
+    const [statusTimer, setStatusTimer] = useState<boolean>(false)
 
     const stop = () => {
-        // stop
+        clearInterval(timerId)
+        setStatusTimer(false)
     }
     const start = () => {
-        stop()
         const id: number = window.setInterval(() => {
-            // setDate
+            setDate(new Date().toLocaleString())
         }, 1000)
         setTimerId(id)
+        setStatusTimer(true)
     }
 
     const onMouseEnter = () => {
-        // show
+        setShow(true)
     }
     const onMouseLeave = () => {
-        // close
+        setShow(false)
     }
-
-    const stringTime = 'Time' // fix with date
-    const stringDate = 'Date' // fix with date
+    const arrDate = date.split(',')
+    const stringTime = arrDate.length > 1 ? arrDate[1] : '00:00:00'
+    const stringDate =arrDate.length > 1 ? arrDate[0] : '00.00.0000'
 
     return (
         <div>
-            <div
-                onMouseEnter={onMouseEnter}
-                onMouseLeave={onMouseLeave}
-            >
-                {stringTime}
+            <div className={s.wrapper__time__data}>
+                <div
+                    onMouseEnter={onMouseEnter}
+                    onMouseLeave={onMouseLeave}
+                    className={s.clock__time}
+                >
+                    <b>{stringTime}</b>
+                </div>
+
+                {show && (
+                    <div className={s.clock__data}>
+
+                        <b>&nbsp;&nbsp;Date: {stringDate}</b>
+                    </div>
+                )}
             </div>
 
-            {show && (
-                <div>
-                    {stringDate}
-                </div>
-            )}
+            <div className={s.wrapper__manage}>
 
-            <SuperButton onClick={start}>start</SuperButton>
-            <SuperButton onClick={stop}>stop</SuperButton>
+                <SuperButton onClick={start}>start</SuperButton>
+                <SuperButton onClick={stop}>stop</SuperButton>
+                {statusTimer ?
+                    <div className={s.started}>Запущен</div> :
+                    <div className={s.stoped}>Остановлен</div>
+                }
+            </div>
 
         </div>
     )
